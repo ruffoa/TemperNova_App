3c747f223dacdd3d6f6129250e44eb981bf5bf32
ruffoa <ruffo.alex@gmail.com>
2020-02-08 13:28:33 -0500

    Fix a few minor bugs, and implement refills logic

--- app/src/main/java/com/ruffo/tempernova/helpers/Temperature.kt
+++ app/src/main/java/com/ruffo/tempernova/helpers/Temperature.kt
@@ -3,10 +3,12 @@ package com.ruffo.tempernova.helpers
 import com.ruffo.tempernova.MainActivity
 import com.ruffo.tempernova.R
 import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 
 class Temperature {
     private var avgTemp = -1
     private var pastTemp = mutableListOf<Int>()
+    private var wasCool = AtomicBoolean(true)
 
     private val numToAverage = 5
     private val thresholdTemp = 80
@@ -19,20 +21,27 @@ class Temperature {
         val date: Date = date
     }
 
-    fun updateAvgTemp(temp: Int) {
-        if (temp > -100) {  // sensor gives < -100 values as error codes, so ignore it if we get one!
+    fun updateAvgTemp(temp: Int?) {
+        if (temp !== null && temp > -100) {  // sensor gives < -100 values as error codes, so ignore it if we get one!
             checkIfRefilled(temp) // check if the user refilled the mug
 
             pastTemp.add(temp)
             if (pastTemp.size > numToAverage)
                 pastTemp = pastTemp.drop(1).toMutableList()    // drop the first element!
 
-            avgTemp = pastTemp.sum() / numToAverage
+            avgTemp =  temp
+
+            if (pastTemp.isNotEmpty()) {
+                avgTemp = pastTemp.sum() / numToAverage
+            }
+
         }
     }
 
-    fun checkIfRefilled(temp: Int) {
-        if (temp > thresholdTemp && avgTemp < coolThresholdTemp) {
+    private fun checkIfRefilled(temp: Int) {
+        if (temp > thresholdTemp && (avgTemp >= coolThresholdTemp && wasCool.get())) {
+            wasCool.set(false)
+
             val context = MainActivity.CoreHelper.contextGetter?.invoke() as MainActivity
 
             if (context !== null) {
